3c696dd98f96432b9ca9622219d2605309d9d2f2
Justice Nyaga <justiceevansmwema@gmail.com>
2024-03-05 13:02:53 +0300

    Create a custom TextInput component

---
+++ components/TextInput.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { View, StyleSheet, TextInput } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import PropTypes from "prop-types";
+
+import defaultStyles from "../config/styles";
+
+const AppTextInput = ({ icon, width = "100%", ...rest }) => {
+  return (
+    <View style={[styles.container, { width }]}>
+      {icon && (
+        <MaterialCommunityIcons
+          name={icon}
+          size={20}
+          color={defaultStyles.colors.medium}
+          style={styles.icon}
+        />
+      )}
+      <TextInput
+        placeholderTextColor={defaultStyles.colors.medium}
+        style={[defaultStyles.text, { flex: 1 }]}
+        {...rest}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: defaultStyles.colors.light,
+    borderRadius: 25,
+    padding: 15,
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 10,
+  },
+  icon: {
+    marginRight: 10,
+  },
+});
+
+AppTextInput.propTypes = {
+  icon: PropTypes.string,
+  width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
+
+export default AppTextInput;
