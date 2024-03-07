b060b3484e90c99b885a34a5180c6b917c7f686e
Justice Nyaga <justiceevansmwema@gmail.com>
2024-03-07 12:11:02 +0300

    Implement auth secure store API

--- App.js
+++ App.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 
-import AuthNavigator from "./app/navigation/AuthNavigator";
+import authStorage from "./app/auth/storage";
 import navigationTheme from "./app/navigation/navigationTheme";
+import AuthContext from "./app/auth/context";
+import AuthNavigator from "./app/navigation/AuthNavigator";
+import AppNavigator from "./app/navigation/AppNavigator";
 
 const App = () => {
+  const [user, setUser] = useState();
+
+  const restoreUser = async () => {
+    const user = await authStorage.getUser();
+    setUser(user);
+  };
+
+  useEffect(() => {
+    restoreUser();
+  }, []);
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <NavigationContainer theme={navigationTheme}>
-        <AuthNavigator />
-      </NavigationContainer>
+      <AuthContext.Provider value={{ user, setUser }}>
+        <NavigationContainer theme={navigationTheme}>
+          {user ? <AppNavigator /> : <AuthNavigator />}
+        </NavigationContainer>
+      </AuthContext.Provider>
     </GestureHandlerRootView>
   );
 };
---
+++ app/auth/storage.js
@@ -0,0 +1,35 @@
+import * as Storage from "expo-secure-store";
+import { jwtDecode } from "jwt-decode";
+
+const key = "authToken";
+
+const storeToken = async (authToken) => {
+  try {
+    await Storage.setItemAsync(key, authToken);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getToken = async () => {
+  try {
+    return await Storage.getItemAsync(key);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getUser = async () => {
+  const token = await getToken();
+  return token ? jwtDecode(token) : null;
+};
+
+const removeToken = async () => {
+  try {
+    await Storage.deleteItemAsync(key);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default { getToken, getUser, removeToken, storeToken };
