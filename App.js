e8c9e0e8c148f95d071a5dd8ce777696e2d1ec62
Justice Nyaga <justiceevansmwema@gmail.com>
2024-03-07 17:58:13 +0300

    Display Splash screen as the app loads resources

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useCallback, useState, useEffect } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import "core-js/stable/atob";
+import * as SplashScreen from "expo-splash-screen";
 
 import authStorage from "./app/auth/storage";
 import navigationTheme from "./app/navigation/navigationTheme";
@@ -9,20 +10,35 @@ import AuthContext from "./app/auth/context";
 import AuthNavigator from "./app/navigation/AuthNavigator";
 import AppNavigator from "./app/navigation/AppNavigator";
 
+// Keep the splash screen visible while we fetch the resources
+SplashScreen.preventAutoHideAsync();
+
 const App = () => {
   const [user, setUser] = useState();
+  const [isReady, setIsReady] = useState(false);
 
   const restoreUser = async () => {
     const user = await authStorage.getUser();
     setUser(user);
+    setIsReady(true);
   };
 
   useEffect(() => {
     restoreUser();
   }, []);
 
+  const onLayoutRootView = useCallback(async () => {
+    if (isReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [isReady]);
+
+  if (!isReady) {
+    return null;
+  }
+
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
+    <GestureHandlerRootView style={{ flex: 1 }} onLayout={onLayoutRootView}>
       <AuthContext.Provider value={{ user, setUser }}>
         <NavigationContainer theme={navigationTheme}>
           {user ? <AppNavigator /> : <AuthNavigator />}
