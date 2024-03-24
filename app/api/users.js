fa7aafe5cd522dee2a5e71b35dfd53eaac891cd1
Justice Nyaga <justiceevansmwema@gmail.com>
2024-03-24 17:02:00 +0300

    Add resend-verificaition and refresh-token apis

--- app/api/users.js
+++ app/api/users.js
@@ -1,7 +1,16 @@
 import client from "./client";
 
-const register = (userInfo) => client.post("/users", userInfo);
+const endpoint = "/users";
+
+const register = (userInfo) => client.post(endpoint, userInfo);
+
+const resendVerification = (email) =>
+  client.post(endpoint + "/resend-verification", { email });
+
+const refreshAuthToken = () => client.post(endpoint + "/refresh-token");
 
 export default {
+  refreshAuthToken,
   register,
+  resendVerification,
 };
