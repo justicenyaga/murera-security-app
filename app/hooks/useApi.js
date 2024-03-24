27ff241c82098eb8d3f0cd4da6ae23dfe7de5501
Justice Nyaga <justiceevansmwema@gmail.com>
2024-03-25 00:55:12 +0300

    Return response headers on api hook

--- app/hooks/useApi.js
+++ app/hooks/useApi.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 export default function (apiFunc) {
   const [data, setData] = useState();
+  const [headers, setHeaders] = useState();
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
 
@@ -12,8 +13,9 @@ export default function (apiFunc) {
 
     setError(!response.ok);
     setData(response.data);
+    setHeaders(response.headers);
     return response;
   };
 
-  return { data, error, loading, request };
+  return { data, error, headers, loading, request };
 }
