6002d77e19db448c9df4a4bdf03a2edf20d697ad
Justice Nyaga <justiceevansmwema@gmail.com>
2024-03-24 17:23:18 +0300

    Redirect inActive users to activation screen

--- app/navigation/AuthNavigator.js
+++ app/navigation/AuthNavigator.js
@@ -1,24 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
+import { useNavigation } from "@react-navigation/native";
 
 import routes from "./routes";
+import useAuth from "../auth/useAuth";
 
-import WelcomeScreen from "../screens/WelcomeScreen";
+import ActivationScreen from "../screens/ActivationScreen.js";
 import LoginScreen from "../screens/LoginScreen";
 import RegisterScreen from "../screens/RegisterScreen";
+import WelcomeScreen from "../screens/WelcomeScreen";
 
 const Stack = createStackNavigator();
 
-const AuthNavigator = () => (
-  <Stack.Navigator>
-    <Stack.Screen
-      name={routes.WELCOME}
-      component={WelcomeScreen}
-      options={{ headerShown: false }}
-    />
-    <Stack.Screen name={routes.LOGIN} component={LoginScreen} />
-    <Stack.Screen name={routes.REGISTER} component={RegisterScreen} />
-  </Stack.Navigator>
-);
+const AuthNavigator = () => {
+  const navigation = useNavigation();
+  const { user } = useAuth();
+
+  useEffect(() => {
+    !user.isActive && navigation.navigate(routes.ACTIVATION);
+  });
+
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name={routes.WELCOME}
+        component={WelcomeScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen name={routes.LOGIN} component={LoginScreen} />
+      <Stack.Screen name={routes.REGISTER} component={RegisterScreen} />
+      <Stack.Screen
+        name={routes.ACTIVATION}
+        component={ActivationScreen}
+        options={{ title: "" }}
+      />
+    </Stack.Navigator>
+  );
+};
 
 export default AuthNavigator;
--- app/navigation/routes.js
+++ app/navigation/routes.js
@@ -1,4 +1,5 @@
 export default Object.freeze({
+  ACTIVATION: "Activation",
   LOGIN: "Login",
   REGISTER: "Register",
   WELCOME: "Welcome",
