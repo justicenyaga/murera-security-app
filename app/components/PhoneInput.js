3c3dd187aaa72a603cf8e3c031deb3fb4e6da5be
Justice Nyaga <justiceevansmwema@gmail.com>
2024-04-02 15:54:02 +0300

    Create a custom phone number input component

---
+++ app/components/PhoneInput.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import PhoneInput from "react-native-phone-number-input";
+import PropTypes from "prop-types";
+
+import defaultStyles from "../config/styles";
+
+const AppPhoneInput = ({
+  placeholder,
+  setCode,
+  setValue,
+  value,
+  width = "100%",
+}) => {
+  const handleChangeCode = ({ callingCode }) => setCode(callingCode);
+
+  return (
+    <PhoneInput
+      containerStyle={[styles.container, { width }]}
+      codeTextStyle={defaultStyles.text}
+      textContainerStyle={styles.textContainer}
+      defaultValue={value}
+      defaultCode="KE"
+      layout="first"
+      textInputStyle={defaultStyles.text}
+      textInputProps={{
+        placeholder,
+        maxLength: 9,
+        placeholderTextColor: defaultStyles.colors.medium,
+      }}
+      onChangeCountry={handleChangeCode}
+      onChangeText={setValue}
+      autoFocus
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: defaultStyles.colors.light,
+    borderRadius: 25,
+    marginVertical: 10,
+  },
+  textContainer: {
+    backgroundColor: defaultStyles.colors.light,
+    borderRadius: 25,
+  },
+});
+
+AppPhoneInput.propTypes = {
+  placeholder: PropTypes.string,
+  setCode: PropTypes.func,
+  setValue: PropTypes.func,
+  value: PropTypes.string,
+  width: PropTypes.string,
+};
+
+export default AppPhoneInput;
