52a98d4ddf82cbf43acac4088ea2a0f7e37ad69b
Justice Nyaga <justiceevansmwema@gmail.com>
2024-04-03 19:35:10 +0300

    Implement a custom form phone input field

---
+++ app/components/forms/FormPhoneInput.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useFormikContext } from "formik";
+import PropTypes from "prop-types";
+
+import ErrorMessage from "./ErrorMessage";
+import PhoneInput from "../PhoneInput";
+
+const FormPhoneInput = ({ codeName, name, placeholder, width }) => {
+  const { errors, setFieldValue, touched, values } = useFormikContext();
+
+  return (
+    <>
+      <PhoneInput
+        placeholder={placeholder}
+        width={width}
+        setCode={(text) => setFieldValue(codeName, text)}
+        setValue={(text) => setFieldValue(name, text)}
+        value={values[name]}
+      />
+      <ErrorMessage error={errors[name]} visible={touched[name]} />
+    </>
+  );
+};
+
+FormPhoneInput.propTypes = {
+  codeName: PropTypes.string,
+  name: PropTypes.string,
+  placeholder: PropTypes.string,
+  width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
+
+export default FormPhoneInput;
--- app/components/forms/index.js
+++ app/components/forms/index.js
@@ -2,4 +2,5 @@ export { default as ErrorMessage } from "./ErrorMessage";
 export { default as Form } from "./Form";
 export { default as FormDatePicker } from "./FormDatePicker";
 export { default as FormField } from "./FormField";
+export { default as FormPhoneInput } from "./FormPhoneInput";
 export { default as SubmitButton } from "./SubmitButton";
