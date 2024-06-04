5c042a8655767a6e1082080945a2515f569d8441
Justice Nyaga <justiceevansmwema@gmail.com>
2024-06-04 14:51:57 +0300

    Add report emergency button on home screen

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,27 +1,49 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
+import Button from "../components/Button";
 import Screen from "../components/Screen";
 import UserCard from "../components/UserCard";
 
+import colors from "../config/colors";
+import routes from "../navigation/routes";
+
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
   return (
     <Screen style={styles.screen}>
       <View style={styles.container}>
         <UserCard />
+
+        <Button
+          title="Report Emergency"
+          icon="alarm-light-outline"
+          textStyle={styles.reportBtnText}
+          style={styles.button}
+          onPress={() => navigation.navigate(routes.NEW_CASE)}
+        />
       </View>
     </Screen>
   );
 };
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    padding: 15,
+  button: {
+    marginVertical: 30,
   },
   container: {
     paddingTop: 20,
     width: "100%",
+  },
+  screen: {
+    backgroundColor: colors.light,
+    flex: 1,
+    padding: 15,
+  },
+  reportBtnText: {
+    textTransform: "none",
   },
 });
 
