3ec670386709cc9fe772a89409984dc5e3cbf2ed
Justice Nyaga <justiceevansmwema@gmail.com>
2024-06-11 17:02:21 +0300

    Display pending cases on home screen

---
+++ app/components/CaseCard.js
@@ -0,0 +1,96 @@
+import React from "react";
+import { View, StyleSheet, TouchableWithoutFeedback } from "react-native";
+import PropTypes from "prop-types";
+
+import Text from "./Text";
+
+import colors from "../config/colors";
+
+const CaseCard = ({ caseDetails, onPress }) => {
+  const date = new Date(caseDetails.createdAt).toLocaleDateString("en-GB");
+  const time = new Date(caseDetails.createdAt).toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+  const dateTime = `${date} ${time}`;
+
+  const statusStyles = { backgroundColor: "#9e9e9e", color: colors.white };
+  switch (caseDetails.status) {
+    case "Pending":
+      statusStyles.backgroundColor = "#ffeb3b";
+      statusStyles.color = colors.medium;
+      break;
+    case "InProgress":
+      statusStyles.backgroundColor = "#4caf50";
+      break;
+  }
+
+  return (
+    <TouchableWithoutFeedback onPress={onPress}>
+      <View style={styles.container}>
+        <View style={styles.row}>
+          <Text style={styles.title} numberOfLines={1}>
+            {caseDetails.title}
+          </Text>
+          <Text style={styles.caseId} numberOfLines={1}>
+            {caseDetails._id}
+          </Text>
+        </View>
+        <Text style={styles.station} numberOfLines={1}>
+          {caseDetails.station.name}
+        </Text>
+        <View style={styles.row}>
+          <Text style={styles.dateTime}>{dateTime}</Text>
+          <Text style={[styles.status, statusStyles]}>
+            {caseDetails.status}
+          </Text>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+const styles = StyleSheet.create({
+  caseId: {
+    color: colors.medium,
+    fontSize: 10,
+    textAlign: "right",
+    width: "39%",
+  },
+  container: {
+    backgroundColor: colors.white,
+    borderRadius: 15,
+    marginVertical: 5,
+    padding: 10,
+  },
+  dateTime: {
+    fontSize: 14,
+  },
+  station: {
+    marginVertical: 3,
+  },
+  status: {
+    borderRadius: 15,
+    color: colors.white,
+    fontSize: 14,
+    paddingHorizontal: 10,
+    paddingVertical: 2,
+  },
+  title: {
+    fontWeight: "bold",
+    width: "60%",
+  },
+  row: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+});
+
+CaseCard.propTypes = {
+  caseDetails: PropTypes.object.isRequired,
+  onPress: PropTypes.func.isRequired,
+};
+
+export default CaseCard;
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,16 +1,32 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { FlatList, StyleSheet, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 import Button from "../components/Button";
+import CaseCard from "../components/CaseCard";
 import Screen from "../components/Screen";
+import Text from "../components/Text";
 import UserCard from "../components/UserCard";
 
 import colors from "../config/colors";
 import routes from "../navigation/routes";
+import useCases from "../hooks/useCases";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
+  const { cases, getCases } = useCases();
+
+  const [refreshingCases, setRefreshingCases] = useState(false);
+
+  const refreshCases = async () => {
+    setRefreshingCases(true);
+    await getCases();
+    setRefreshingCases(false);
+  };
+
+  useEffect(() => {
+    getCases();
+  }, [cases.all.length]);
 
   return (
     <Screen style={styles.screen}>
@@ -24,6 +40,25 @@ const HomeScreen = () => {
           style={styles.button}
           onPress={() => navigation.navigate(routes.NEW_CASE)}
         />
+
+        {cases.ongoing.length > 0 && (
+          <>
+            <Text style={styles.casesHeading}>Open Cases</Text>
+            <FlatList
+              data={cases.ongoing}
+              keyExtractor={(item) => item._id.toString()}
+              maxHeight={"63%"}
+              onRefresh={refreshCases}
+              refreshing={refreshingCases}
+              renderItem={({ item }) => (
+                <CaseCard
+                  caseDetails={item}
+                  onPress={() => navigation.navigate(routes.CASE_DETAILS, item)}
+                />
+              )}
+            />
+          </>
+        )}
       </View>
     </Screen>
   );
@@ -32,6 +67,12 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   button: {
     marginVertical: 30,
+  },
+  casesHeading: {
+    fontWeight: "bold",
+    marginBottom: 10,
+    textAlign: "center",
+    textTransform: "uppercase",
   },
   container: {
     paddingTop: 20,
